9690d572164501528ac25d8a48678443643ebd1f
tania <taniaosano15@gmail.com>
2022-08-21 00:19:04 +0300

    Completed functions lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(block){
+     return Math.abs(block - 42);
+}
+
+function distanceFromHqInFeet(feet){
+     return distanceFromHqInBlocks(feet) * 264;
+}
+
+function distanceTravelledInFeet(street1, street2){
+     return Math.abs(street2 - street1)*264;
+}
+
+function calculatesFarePrice(street1, street2){
+     let distance = distanceTravelledInFeet(street1, street2);
+     if(distance < 400){
+          return 0;
+     }
+     else if(distance > 400 && distance <= 2000){
+          return (distance - 400) * 0.02;
+     }
+     else if(distance > 2000 && distance <= 2500){
+          return 25;
+     }
+     else {
+          return "cannot travel that far";
+     }
+}
+     
